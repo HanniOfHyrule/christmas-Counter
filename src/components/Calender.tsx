ffb29364e633f866cbe933bbc7166fabad764bca
Hanni Liebler <johanna.liebler@gmail.com>
2022-12-20 14:09:26 +0100

    add new component Calender

---
+++ src/components/Calender.tsx
@@ -0,0 +1,15 @@
+import backgroundImage from "../assets/img/annie-spratt-QKo-op_gR9I-unsplash.png";
+import { Outlet } from "react-router-dom";
+
+const Calender = () => {
+  return (
+    <div>
+      {/* <div
+        className="image"
+        style={{ backgroundImage: `url("${backgroundImage}")` }}
+      ></div> */}
+    </div>
+  );
+};
+
+export default Calender;
