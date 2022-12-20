aa72f708c6f8e2a06a47ca74b1955d32bc0146dd
Hanni Liebler <johanna.liebler@gmail.com>
2022-12-20 14:10:08 +0100

    add some styling and implement useNavigate for the routing

--- src/components/CountDown.tsx
+++ src/components/CountDown.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import "./countdown.css";
+import "./Countdown.css";
 import backgroundImage from "../assets/img/annie-spratt-QKo-op_gR9I-unsplash.png";
+import { Outlet, useNavigate } from "react-router-dom";
 
 const CountDown = () => {
   const [days, setDays] = useState<string | number>("H");
@@ -31,49 +32,45 @@ const CountDown = () => {
 
   setInterval(coutner, 1000);
 
+  const navigate = useNavigate();
+
   return (
     <React.Fragment>
       {" "}
-      <div
+      {/* <div
         className="image"
         style={{ backgroundImage: `url("${backgroundImage}")` }}
-      >
-        {" "}
-        <style></style>
-        <div></div>
-        <section className="container">
-          <h1>Christmas Countdown</h1>
-          <div className="countdown">
-            <article>
-              <p className="count_time">{days}</p>
-              <p className="date">days</p>
-            </article>
-            <article>
-              <p className="count_time">{hours}</p>
-              <p className="date">hours</p>
-            </article>
-            <article>
-              <p className="count_time">{minutes}</p>
-              <p className="date">min</p>
-            </article>
-            <article>
-              <p className="count_time">{seconds}</p>
-              <p className="date">sec</p>
-            </article>
-          </div>
-          <label className="calender">
-            Coming soon, add your own advent calender!
-          </label>
-          <button
-            className="calender_button"
-            onClick={(event: any) => {
-              alert("Sorry this site are still under construction!");
-            }}
-          >
-            customize your advent calendar
-          </button>
-        </section>
-      </div>
+      > */}{" "}
+      <style></style>
+      <div></div>
+      <section className="container">
+        <h1>Christmas Countdown</h1>
+        <div className="countdown">
+          <article>
+            <p className="count_time">{days}</p>
+            <p className="date">days</p>
+          </article>
+          <article>
+            <p className="count_time">{hours}</p>
+            <p className="date">hours</p>
+          </article>
+          <article>
+            <p className="count_time">{minutes}</p>
+            <p className="date">min</p>
+          </article>
+          <article>
+            <p className="count_time">{seconds}</p>
+            <p className="date">sec</p>
+          </article>
+        </div>
+        <label className="calender">
+          Coming soon, add your own advent calender!
+        </label>
+        <button className="calender_button" onClick={() => navigate("Login")}>
+          customize your advent calendar
+        </button>
+      </section>
+      {/* </div> */}
     </React.Fragment>
   );
 };
