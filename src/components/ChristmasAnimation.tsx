a42f52c72830f942b5f237458c20eaf05095b3c7
Hanni Liebler <[johanna.liebler@gmail.com]>
2022-12-21 14:36:31 +0100

    add the Christmas animation

---
+++ src/components/ChristmasAnimation.tsx
@@ -0,0 +1,13 @@
+import "./ChristmasAnimation.scss"
+import React from "react"
+
+export default function ChristmasAnimation () {
+    return(
+        <React.Fragment> 
+               <p className="titel">Merry Christmas</p>
+            <div className="xmasTree">
+             <p className="second_titel">and <br/> a happy new year!</p>
+            </div>
+        </React.Fragment>
+    )
+}
