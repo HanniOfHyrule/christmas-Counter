61d793aceb0f1920eae4c8be94b4fcaae873a17f
Hanni Liebler <johanna.liebler@gmail.com>
2023-01-18 09:16:50 +0100

    fix date bug and christmas animation

--- src/components/ChristmasAnimation.tsx
+++ src/components/ChristmasAnimation.tsx
@@ -1,13 +1,15 @@
-import "./ChristmasAnimation.scss"
-import React from "react"
+import "./ChristmasAnimation.scss";
+import React from "react";
 
-export default function ChristmasAnimation () {
-    return(
-        <React.Fragment> 
-               <p className="titel">Merry Christmas</p>
-            <div className="xmasTree">
-             <p className="second_titel">and <br/> a happy new year!</p>
-            </div>
-        </React.Fragment>
-    )
+export default function ChristmasAnimation() {
+  return (
+    <div className="container_animation">
+      <p className="titel">Merry Christmas</p>
+      <div className="xmasTree">
+        <p className="second_titel">
+          and <br /> a happy new year!
+        </p>
+      </div>
+    </div>
+  );
 }
